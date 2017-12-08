a49d15b7109b98f7f9c5ba9107a370d67a5f6c88
drsaeva <drsaeva@gmail.com>
2017-12-07 23:53:17 -0500

    updated dagre_test.html and others

--- dagre.js
+++ dagre.js
@@ -1,3 +1,12 @@
+var g = new dagreD3.graphlib.Graph().setGraph({});
+
+// States and transitions from RFC 793
+/*var states = [ "CLOSED", "LISTEN", "SYN RCVD", "SYN SENT",
+               "ESTAB", "FINWAIT-1", "CLOSE WAIT", "FINWAIT-2",
+               "CLOSING", "LAST-ACK", "TIME WAIT" ];*/
+
+
+var states = [ "War God Descendant", "League of Iron Preparation", "Tiger Warrior Training Technique"];
 var war = {
   nodes: [
       {id: 'wgd', label: "War God Descendant"},
@@ -42,44 +51,41 @@ var war = {
     ]
 }
 
-/// Graph code
-let g = new dagreD3.graphlib.Graph().setGraph({ directed: true });
-
-// nodes
-war.nodes.forEach( function(charm) {
-  g.setNode(charm.id, charm.label);
-});
-g.nodes().forEach( function(n) {
-    console.log(n);
-});
+// pass node/edge data into the graph
+war.nodes.forEach(function(charm) { g.setNode(charm.id, { label: charm.label }); });
+war.edges.forEach(function(edge) { g.setEdge(edge.from, edge.to, {label: ""}) });
 
 
-// edges
-war.edges.forEach( function(chedge) {
-  g.setEdge(chedge.from, chedge.to, "");
-
-});
-g.edges().forEach( function(n) {
-    console.log(n);
+// Set some general styles
+g.nodes().forEach(function(v) {
+  var node = g.node(v);
+  //node.rx = node.ry = 5;
 });
 
+// Add some custom colors based on state
+//g.node('CLOSED').style = "fill: #f77";
+//g.node('ESTAB').style = "fill: #7f7";
 
 var svg = d3.select("svg"),
     inner = svg.select("g");
 
-// Zoom support
-var zoom = d3.zoom().on("zoom", function() {
-  inner.attr("transform", "translate(" +d3.event.translate + ")" +
-                "scale(" + d3.event.scale + ")" );
-});
+// Set up zoom support
+var zoom = d3.behavior.zoom().on("zoom", function() {
+      inner.attr("transform", "translate(" + d3.event.translate + ")" +
+                                  "scale(" + d3.event.scale + ")");
+    });
 svg.call(zoom);
 
-// Renderer code
-var renderer = new dagreD3.render("graph");
-renderer(inner, g);
+// Create the renderer
+var render = new dagreD3.render();
 
+// Run the renderer. This is what draws the final graph.
+render(inner, g);
+
+// Center the graph
 var initialScale = 0.75;
-zoom.translate([(svg.attr("width") + g.graph().width * initialScale) / 2, 20])
-    .scale(initialScale)
-    .event(svg);
+zoom
+  .translate([(svg.attr("width") - g.graph().width * initialScale) / 2, 20])
+  .scale(initialScale)
+  .event(svg);
 svg.attr('height', g.graph().height * initialScale + 40);
